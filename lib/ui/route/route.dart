c5a4d0bbead789351d111299f930a4973828f0af
nazmulhasan <nazmul552123@gmail.com>
2023-09-20 00:32:05 +0600

    first commit

--- lib/ui/route/route.dart
+++ lib/ui/route/route.dart
@@ -1,9 +1,14 @@
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:tourapp/ui/views/auth/sing_in.dart';
 import 'package:tourapp/ui/views/auth/sing_up.dart';
+import 'package:tourapp/ui/views/bottomNavController/details_screen.dart';
+import 'package:tourapp/ui/views/bottomNavController/pages/nav_add_last_step.dart';
+import 'package:tourapp/ui/views/bottomNavController/searchscreen.dart';
+import 'package:tourapp/ui/views/bottomNavController/see_all.dart';
 import 'package:tourapp/ui/views/drawer_pages/faq.dart';
 import 'package:tourapp/ui/views/drawer_pages/how_to_use.dart';
 import 'package:tourapp/ui/views/drawer_pages/privacy.dart';
+import 'package:tourapp/ui/views/drawer_pages/profile.dart';
 import 'package:tourapp/ui/views/drawer_pages/setting.dart';
 import 'package:tourapp/ui/views/drawer_pages/support.dart';
 import 'package:tourapp/ui/views/main_home_screen.dart';
@@ -24,6 +29,12 @@ const String howToUseScreen = '/how-to-use-screen';
 const String privacyScreen = '/privacy-screen';
 const String settingScreen = '/setting-screen';
 const String supportScreen = '/support-screen';
+const String searchScreen = '/search-screen';
+const String seeAllScreen = '/see-all-screen';
+const String detailsScreen = '/details-screen';
+const String navAddLastStep = '/Nav-Add-Last-Step-screen';
+const String userProfile = '/user-profile-screen';
+const String setting = '/setting-screen';
 
 // Control our page route flow
 List<GetPage> getpages = [
@@ -39,4 +50,10 @@ List<GetPage> getpages = [
   GetPage(name: privacyScreen, page: () => Privacy()),
   GetPage(name: settingScreen, page: () => Setting()),
   GetPage(name: supportScreen, page: () => Support()),
+  GetPage(name: searchScreen, page: () => SearchScreen()),
+  GetPage(name: seeAllScreen, page: () => SeeAllScreen()),
+  GetPage(name: detailsScreen, page: () => DetailsScreen()),
+  GetPage(name: navAddLastStep, page: () => NavAddLastStep()),
+  GetPage(name: userProfile, page: () => UserProfile()),
+  GetPage(name: setting, page: () => Setting()),
 ];
--- lib/ui/views/bottomNavController/pages/nav_favorite.dart
+++ lib/ui/views/bottomNavController/pages/nav_favorite.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NavFavorite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 15.h),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (_, index) {
+            return Card(
+              elevation: 3,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 200.h,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/back.jpg"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: CircleAvatar(
+                      radius: 25.r,
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.remove_circle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
